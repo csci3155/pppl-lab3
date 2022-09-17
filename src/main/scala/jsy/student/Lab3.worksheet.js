8b8841b698e250d7fb3085e00d1a0fc78bf7f232
mango <fontenot.christian@outlook.com>
2022-09-17 15:52:19 -0600

    Fix worksheet name, add js worksheet

---
+++ src/main/scala/jsy/student/Lab3.worksheet.js
@@ -0,0 +1,11 @@
+/*
+ * CSCI 3155: Lab 3 JavaScript Worksheet
+ *
+ * This worksheet is a place to experiment with JavaScript expressions.
+ */
+
+function id(x) {return x;}
+
+const j = 3; j + id("H");
+
+console.log(id(3*undefined));
